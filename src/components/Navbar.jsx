fa85cb9a540613ff81740b6814318efdbf557257
nerwinalamas <nerwinalamas@gmail.com>
2024-12-18 20:30:33 +0800

    refactor: fixed at top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
     );
 
     return (
-        <header className="border-b">
+        <header className="fixed top-0 left-0 w-full border-b bg-white z-50">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
